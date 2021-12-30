c2f69bc94e85af2a88330112fe30a0f36d3c8ca8
Mudassir Ali <muddassir619@gmail.com>
2021-12-30 19:36:24 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+// DOM Elements
+const time = document.getElementById("clock");
+
+// setinterval (timer), showTime(function),
+//  1 sec = 1000 milliseconds
+setInterval(showTime, 1000);
+        function showTime() {
+            let time = new Date();
+            let hour = time.getHours();
+            let min = time.getMinutes();
+            let sec = time.getSeconds();
+            am_pm = "AM";
+  
+            if (hour > 12) {
+                hour -= 12;
+                am_pm = "PM";
+            }
+            if (hour == 0) {
+                hr = 12;
+                am_pm = "AM";
+            }
+  
+            hour = hour < 10 ? "0" + hour : hour;
+            min = min < 10 ? "0" + min : min;
+            sec = sec < 10 ? "0" + sec : sec;
+  
+            let currentTime = hour + ":" 
+                + min + ":" + sec + am_pm;
+  
+            document.getElementById("clock")
+                .innerHTML = currentTime;
+        }
+  
+        showTime();
+
+
+// Set Background and Greeting
+
+
+// Set Name
+
+// Get Focus
+
+// Set Focus
+
+
+
+// Run
+// showTime();
+// setBgGreet();
+// getName();
+// getFocus();
